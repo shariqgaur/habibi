bb62ff1bb8685fc3a7f341386e2a84ef6d523770
shariq <shariq.mohd09@gmail.com>
2020-06-24 15:41:53 +0400

    first commit

---
+++ backend/src/main/java/com/shekhargulati/app/Application.java
@@ -0,0 +1,69 @@
+package com.shekhargulati.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.lang.reflect.Method;
+
+@SpringBootApplication
+public class Application {
+
+    @Value("${rest.api.base.path}")
+    private String restApiBasePath;
+    @Value("${cors.allowed.origins}")
+    private String[] corsAllowedOrigins;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins(corsAllowedOrigins);
+            }
+        };
+    }
+
+    @Bean
+    public WebMvcRegistrations webMvcRegistrationsHandlerMapping() {
+        Application application = this;
+        return new WebMvcRegistrations() {
+            @Override
+            public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+                return new RequestMappingHandlerMapping() {
+
+                    @Override
+                    protected void registerHandlerMethod(Object handler, Method method, RequestMappingInfo mapping) {
+                        Class<?> beanType = method.getDeclaringClass();
+                        RestController restApiController = beanType.getAnnotation(RestController.class);
+                        if (restApiController != null) {
+                            PatternsRequestCondition apiPattern = new PatternsRequestCondition(application.restApiBasePath)
+                                    .combine(mapping.getPatternsCondition());
+
+                            mapping = new RequestMappingInfo(mapping.getName(), apiPattern,
+                                    mapping.getMethodsCondition(), mapping.getParamsCondition(),
+                                    mapping.getHeadersCondition(), mapping.getConsumesCondition(),
+                                    mapping.getProducesCondition(), mapping.getCustomCondition());
+                        }
+
+                        super.registerHandlerMethod(handler, method, mapping);
+                    }
+                };
+            }
+        };
+    }
+}
